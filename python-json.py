384e1ea2538e5d8dadc36825494c6e53f4fc0d4c
Juan Miranda <jmirandav2@gmail.com>
2020-03-25 15:41:41 -0300

    Delete python-json.py

--- python-json.py
+++
@@ -1,8 +0,0 @@
-import json
-import urllib.request
-
-
-link = "http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22"
-req  = urllib.request.Request(link)
-
-print(urllib.request.urlopen(req).read)
